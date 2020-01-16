eefea79d2576d7e017cce901d5b258667297f3ef
jacobwicks <jacob.m.wicks@gmail.com>
2020-01-15 16:12:58 -0800

    NavBar index-4 correction

--- src/components/NavBar/complete/index-4.tsx
+++ src/components/NavBar/complete/index-4.tsx
@@ -14,9 +14,6 @@ const NavBar = ({
         <Menu.Item content='Answer Flashcards' 
             active={showScene === SceneTypes.answering}
             onClick={() => setShowScene(SceneTypes.answering)}/>
-        <Menu.Item content='Edit Flashcards'
-            active={showScene === SceneTypes.writing}
-            onClick={() => setShowScene(SceneTypes.writing)}/>
     </Menu>
 
 export default NavBar;
